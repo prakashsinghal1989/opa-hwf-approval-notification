5b7e07b5a8e64f3065bea8bbf1ef6ea80860a77a
lalitabisht <prakashsinghal1989@gmail.com>
2021-10-13 17:32:37 +0530

    Expected Time Data Set Preparation Method

--- data-model-generator/data-generator.js
+++ data-model-generator/data-generator.js
@@ -5,48 +5,65 @@ const assignees = [
   {
     title:"Manager",
     jobLevel:1,
-    approvalLimit:5000
+    noOfApprovals:1,
+    approvalLowerLimit:0,
+    approvalUpperLimit:5000
   },
   {
     title:"Senior Manager",
     jobLevel:2,
-    approvalLimit:10000
+    noOfApprovals:2,
+    approvalLowerLimit:5000,
+    approvalUpperLimit:10000
   },
   {
     title:"Director",
     jobLevel:3,
-    approvalLimit:20000
+    noOfApprovals:3,
+    approvalLowerLimit:10000,
+    approvalUpperLimit:20000
   },
   {
     title:"Senior Director",
     jobLevel:4,
-    approvalLimit:30000
+    noOfApprovals:4,
+    approvalLowerLimit:20000,
+    approvalUpperLimit:30000
   },
   {
     title:"Vice President",
     jobLevel:5,
-    approvalLimit:50000
+    noOfApprovals:5,
+    approvalLowerLimit:30000,
+    approvalUpperLimit:50000
   },
   {
     title:"SVP",
     jobLevel:6,
-    approvalLimit:100000
+    noOfApprovals:6,
+    approvalLowerLimit:50000,
+    approvalUpperLimit:100000
   },
   {
     title:"EVP",
     jobLevel:7,
-    approvalLimit:150000
+    noOfApprovals:7,
+    approvalLowerLimit:100000,
+    approvalUpperLimit:150000
   },
   {
     title:"CEO",
     jobLevel:8,
-    approvalLimit:500000
+    noOfApprovals:8,
+    approvalLowerLimit:150000,
+    approvalUpperLimit:500000
   },
 ];
 const minAmount =100;
 const receiptValidityArray = ["TRUE", "FALSE"];
 const hasAttachmentArray = ["TRUE", "FALSE"];
 const outcomeArray=["APPROVE", "REJECT"];
+const priorityArray = [1,2,3];
 
 /**
  * Method to get the random value from the given collection
@@ -60,21 +77,21 @@ const getRandomValue = (collection) => {
 
 /**
  * Method to get the random amount within the provided limit
- * params : approvalLimit: Integer, min: Integer
+ * params : approvalUpperLimit: Integer, min: Integer
  * return: Integer
  */
-const getAmountWithinLimit = (approvalLimit, min) => {
-  return Math.floor(Math.random() * (approvalLimit - min + 1) ) + min
+const getAmountWithinLimit = (approvalUpperLimit, min) => {
+  return Math.floor(Math.random() * (approvalUpperLimit - min + 1) ) + min
 };
 
 /**
  * Method to get the random amount greater thanthe provided limit
- * params : approvalLimit: Integer
+ * params : approvalUpperLimit: Integer
  * return: Integer
  */
 
-const getAmountOutsideLimit = (approvalLimit) => {
-  return Math.floor(Math.random() * approvalLimit)+approvalLimit
+const getAmountOutsideLimit = (approvalUpperLimit) => {
+  return Math.floor(Math.random() * approvalUpperLimit)+approvalUpperLimit
 };
 
 /**
@@ -88,7 +105,7 @@ const buildAllCasesDataSet = (conditions) => {
   for(i=0;i<dataSetNumber;i++){
     const getRandomAssignee = getRandomValue(assignees);
     const {
-      approvalLimit,
+      approvalUpperLimit,
       jobLevel
     } = getRandomAssignee;
     const shouldApprove = amountTobeInLimit && daysDifferenceinLimit && isAttachementAttached;
@@ -98,7 +115,7 @@ const buildAllCasesDataSet = (conditions) => {
       subtype: getRandomValue(subTypeArray),
       creatorLevel: getRandomValue(creatorLevel),
       jobLevel,
-      amount: amountTobeInLimit ? getAmountWithinLimit(approvalLimit, minAmount) : getAmountOutsideLimit(approvalLimit),
+      amount: amountTobeInLimit ? getAmountWithinLimit(approvalUpperLimit, minAmount) : getAmountOutsideLimit(approvalUpperLimit),
       isValidReceipt:daysDifferenceinLimit ? receiptValidityArray[0] :receiptValidityArray[1],
       hasAttachment:isAttachementAttached ? hasAttachmentArray[0] : hasAttachmentArray[1],
       outcome:shouldApprove ? outcomeArray[0] : outcomeArray[1]
@@ -157,3 +174,57 @@ const createDataSet = () => {
   const csvData= convertToCSV(allCasesDataSet);
   document.getElementById("data-box").value = csvData;
 }
+
+
+/**
+ * Method which gets invoked  to generateExpectedTimeDataSet, invokes buildAllCasesDataSet func internally ,
+ * based on the given conditions.
+ * Once the dataSet is generated , adds it to the textarea input.
+ */
+const createExpectedTimeDataSet = () => {
+  const allCasesDataSet = [
+    ...buildExpectedTimeDataSet({ isAttachmentAttached:true, dataSetNumber:50000}),
+    ...buildExpectedTimeDataSet({ isAttachmentAttached:false, dataSetNumber:1000})
+  ]
+
+  const csvData= convertToCSV(allCasesDataSet);
+  document.getElementById("expected-time-data-box").value = csvData;
+}
+
+/**
+ * Method to generate data set based on the provided conditions
+ * params : conditions: {} Object
+ * return: Array
+ */
+const buildExpectedTimeDataSet = (conditions) => {
+  const {amountTobeInLimit, isAttachmentAttached, dataSetNumber = 10} = conditions
+  const allCasesDataSet = [];
+  for(i=0;i<dataSetNumber;i++){
+    const getRandomAssignee = getRandomValue(assignees);
+    const priority=getRandomValue(priorityArray);
+
+    const {
+      approvalUpperLimit,
+      noOfApprovals,
+      approvalLowerLimit
+    } = getRandomAssignee;
+
+    const amt= getAmountWithinLimit(approvalUpperLimit, approvalLowerLimit);
+    var timeTaken = noOfApprovals;
+    if(!isAttachmentAttached)
+        timeTaken+=1;
+    if(isAttachmentAttached && priority==1)
+        timeTaken=timeTaken/2;
+
+    const dataObj = {
+      processName: processNameText,
+      category: getRandomValue(subTypeArray),
+      amount: amt,
+      hasAttachment:isAttachmentAttached ? hasAttachmentArray[0] : hasAttachmentArray[1],
+      priority:priority,
+      outcome:timeTaken
+    }
+    allCasesDataSet.push(dataObj);
+  }
+  return allCasesDataSet;
+};
